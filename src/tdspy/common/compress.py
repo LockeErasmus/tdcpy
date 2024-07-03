ce9eed7dd9c4cb98056e48b1613bc6b8aace3cd5
Peichl Adam <adpeichl@gmail.com>
2024-07-03 22:22:09 +0200

    sorting function (A, hA) with tests

--- src/tdspy/common/compress.py
+++ src/tdspy/common/compress.py
@@ -18,15 +18,11 @@ def compress_matrices_delays(A: npt.NDArray, hA: npt.NDArray, rtol=1e-5, atol=1e
     """ Compresses matrices - delays representation
     
     Removes delay duplicates, sorts delays into ascending order and removes
-    matrices close to zero, i.e. converts the representation:
+    matrices close to zero, i.e. converts the representation (A, hA):
 
         A[0] x(t-hA[0]) + ... + A[mA] x(t-hA[mA])                           (1)
     
-    into:
-
-        A*[0] x(t-hA*[0]) + ... + A[mA*] x(t-hA*[mA*])                      (2)
-    
-    where:
+    into representation (A*, hA*), where:
         1. matrices A*[i] are NOT close to zero
         1. hA* does not contain duplicates
 
@@ -60,6 +56,34 @@ def compress_matrices_delays(A: npt.NDArray, hA: npt.NDArray, rtol=1e-5, atol=1e
     compressed_hA = unique_hA[~mask]
 
     return compressed_A, compressed_hA
+
+def sort_matrices_delays(A: npt.NDArray, hA: npt.NDArray):
+    """ Sorts matrices - delays representation (ascending order by delays)
+    
+    Sorts delays into ascending order, i.e. changes the representation (A, hA):
+
+        A[0] x(t-hA[0]) + ... + A[mA] x(t-hA[mA])                           (1)
+    
+    into representation (A*, hA*), where:
+        1. hA* is now in ascending order
+        2. shapes are preserved
+
+    Args:
+        A: (array): 3D array of stacked matrics (axis 2)
+        hA: (array): 1D array (vector) of delays
+    
+    Returns:
+        tuple containing:
+
+            - compressed_A (array): compressed representation of A
+            - compressed_hA (array): compressed vector of delays
+    
+    Notes:
+        1. the sort is "stable" (see numpy.argsort implementation)
+    """
+    # Consider adding tests here - TODO
+    sorted_index = np.argsort(hA, kind="stable")
+    return A[:,:,sorted_index], hA[sorted_index]
 
 def compress_ddae():
     raise NotImplementedError(".")
--- test/test_common.py
+++ test/test_common.py
@@ -42,9 +42,30 @@ def test_compression_01():
     assert np.all(compressed_A[:,:,1] == np.eye(n))
     assert np.all(compressed_hA == np.array([0, 2.5]))
 
+def test_sort_empty():
+    """ Tests sort of empty representation """
 
+    A = np.zeros(shape=(4,4,0))
+    hA = np.zeros(shape=(0,))
 
+    sorted_A, sorted_hA = compress_matrices_delays(A, hA)
 
+    assert A.shape == sorted_A.shape
+    assert hA.shape == sorted_hA.shape
+    assert sorted_A.size == 0
+    assert sorted_hA.size == 0
 
+def test_sort_01():
+    """ Tests sort of empty representation """
 
+    n = 4
+    A = [i*np.eye(n, dtype=np.float64) for i in range(1,6)]
+    A = np.stack(A, axis=2)
+    hA = np.array([0.1, 1.5, 0.05, 0.2, 0.8])
 
+    sorted_A, sorted_hA = compress_matrices_delays(A, hA)
+
+    assert A.shape == sorted_A.shape
+    assert hA.shape == sorted_hA.shape
+    assert np.allclose(sorted_hA, np.array([0.05, 0.1, 0.2, 0.8, 1.5]))
+    assert np.allclose(sorted_A, A[:,:,[2, 0, 3, 4, 1]])
