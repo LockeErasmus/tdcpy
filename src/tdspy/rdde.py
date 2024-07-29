10ba91dca7f1c70f457f72caa0915956abc5e146
adriansal0712 <saldanha.adrian@gmail.com>
2024-07-29 21:49:19 +0200

    updated the function rdde.py and the test example example01.py

--- src/tdspy/rdde.py
+++ src/tdspy/rdde.py
@@ -5,11 +5,17 @@ RDDE - Retarded Delay Differential Equation
 Notes:
     1. As of now, only A and hA, rest of the implementation will follow
 """
+import logging
 
 import numpy as np
 import numpy.typing as npt
+from scipy import linalg
 
+from .common.compress import compress_matrices_delays, sort_matrices_delays
 from .base import TDSBase
+
+logger = logging.getLogger(__name__)
+
 
 class RDDE(TDSBase):
     """ Retarded Delay Differential Equaton
@@ -17,42 +23,176 @@ class RDDE(TDSBase):
     TODO documentation
 
     """
-    def __init__(self, A, hA,
-                 B1=None, hB1=None, C1=None, hC1=None, D1=None, hD11=None,
-                 B2=None, hB2=None, C2=None, hC2=None, D12=None, hD12=None, D21=None, hD21=None, D22=None, hD22=None) -> None:
+    def __init__(self, A: npt.NDArray, hA: npt.NDArray, 
+                 B: npt.NDArray=None, hB: npt.NDArray=None, C: npt.NDArray=None, hC: npt.NDArray=None, 
+                 D: npt.NDArray=None, hD: npt.NDArray=None, **kwargs) -> None:
+        """
+        
+        Args: 
+
+
+            **kwargs:
+                dtype (): default np.float64
+                tol_singular (float): considering value singular, default 1e-12
+
+        
+        """
+        # A, hA
+        assert isinstance(A, np.ndarray) and isinstance(hA, np.ndarray), "both ndarrays"
+        assert A.ndim  == 3 and hA.ndim == 1, "dimensions check 1"
+        assert A.shape[2] == hA.shape[0], "number of delays hA does not match number of matrices Ai"
+        assert np.all(hA >= 0.0), "only non-negative delays possible"
+        if not np.any(hA == 0):
+            hA = np.r_[0.0, hA]
+            A = np.concatenate([np.zeros((A.shape[0],A.shape[1], 1), dtype=A.dtype), A], axis = 2)
+
+        # I/O matrices
+        if B is not None or hB is not None:
+            # input matrices are defined
+            assert isinstance(B, np.ndarray) and isinstance(hB, np.ndarray), "both ndarrays"
+            assert B.ndim == 3 and hB.ndim == 1, "dimensions check 1"
+            assert B.shape[0] == A.shape[0], "shapes of system do not match A-B matrices"
+            assert B.shape[2] == hB.shape[0], "number of delays hB do not match number of matrices Bi"
+            assert np.all(hB >= 0.0), "only non-negative delays possible"
+
+        if C is not None or hC is not None:
+            # output matrices are defined
+            assert isinstance(C, np.ndarray) and isinstance(hC, np.ndarray), "both ndarrays"
+            assert C.ndim == 3 and hC.ndim == 1, "dimensions check 1"
+            assert C.shape[1] == A.shape[1], "shapes of system does not match A-C matrices "
+            assert C.shape[2] == hC.shape[0], "number of delays hB does not match number of matrices Bi"
+            assert np.all(hC >= 0.0), "only non-negative delays possible"
+        
+        if D is not None or hD is not None:
+            # feed-through matrices are defined
+            assert isinstance(C, np.ndarray), "C, hC needs to be defined to define D, hD"
+            assert isinstance(D, np.ndarray) and isinstance(hD, np.ndarray), "both ndarrays"
+            assert D.ndim == 3 and hD.ndim == 1, "dimensions check 1"
+            assert D.shape[0] == C.shape[0], "shapes of system does not match C-D matrices "
+            assert D.shape[2] == hD.shape[0], "number of delays hB does not match number of matrices Bi"
+            assert np.all(hD >= 0.0), "only non-negative delays possible"
+
+        # --- ARGS ---
+        ## dynamics
+        self._E = np.eye(A.shape[2])    # check this - is there a better implementation?
+        self._uE = np.eye(A.shape[2])   # check this - is there a better implementation?
+        self._vE = np.eye(A.shape[2])   # check this - is there a better implementation?
+        self._A = A
+        self._hA = hA
+        self._B = B
+        self._hB = hB
+        self._C = C
+        self._hC = hC
+        self._D = D
+        self._hD = hD
+        
+        # TODO checks when for example A, B, C defined and D is not        
+
+        # --- KWARGS ---
+        self.dtype = kwargs.get("dtype", np.float64)
+        self.tol_singular = kwargs.get("tol_singular", 1e-12)
+
+
         super().__init__()
 
         # TODO perform checks
         assert len(A) > 0, "TODO"
-
-        ## dynamics
-        self._A = A
-        self._hA = hA
         
         # TODO rest of the system description
 
     @property
     def n(self) -> int:
-        return self._A[0].shape[0]
+        """ number of variables """
+        return self.A.shape[1]
 
     @property
+    def n_inputs(self) -> int:
+        """ number of inputs """
+        if self.B is None:
+            return 0
+        return self.B.shape[1]
+    
+    @property
+    def n_outputs(self) -> int:
+        """ number of outputs """
+        if self.C is None:
+            return 0
+        return self.C.shape[0]
+    
+    @property
     def E(self) -> npt.NDArray:
-        return np.eye(self.n)
+        return np.eye(self.n, dtype=self.dtype)
     
     @property
     def A(self) -> list[npt.NDArray]:
-        """ list of dynamics matrices """
+        """ dynamics matrices """
         return self._A
     
     @property
     def hA(self) -> list[float]:
-        """ delays """
+        """ dynamic delays """
         return self._hA
     
     @property
     def mA(self) -> int:
         """ number of delays """
-        return len(self.hA)
+        return self.hA.shape[0]
+    
+    @property
+    def B(self) -> npt.NDArray | None:
+        """ input matrices """
+        return self._B
+    
+    @property
+    def hB(self) -> npt.NDArray | None:
+        """ input delays """
+        return self._hB
+    
+    @property
+    def mB(self) -> int:
+        """ number of input delays """
+        if self._hB is None:
+            return 0
+        return self.hB.shape[0]
+    
+    @property
+    def C(self) -> npt.NDArray | None:
+        """ output matrices """
+        return self._C
+    
+    @property
+    def hC(self) -> npt.NDArray | None:
+        """ output delays """
+        return self._hC
+    
+    @property
+    def mC(self) -> int:
+        """ number of output delays """
+        if self._hC is None:
+            return 0
+        return self.hC.shape[0]
+    
+    @property
+    def D(self) -> npt.NDArray | None:
+        """ feed-through matrices """
+        return self._D
+    
+    @property
+    def hD(self) -> npt.NDArray | None:
+        """ feed-through delays """
+        return self._hD
+    
+    @property
+    def mD(self) -> int:
+        """ number of feed-through delays """
+        if self._hD is None:
+            return 0
+        return self.hD.shape[0]
+
+    @property
+    def is_logical(self) -> bool:
+        """ property from original MATLAB package, unused as of now """
+        return False # as of now, always assume DDAE is not logical
     
     @property
     def is_compressed(self) -> bool:
@@ -73,13 +213,57 @@ class RDDE(TDSBase):
         return True # as of now, always assume RDDE is LTI
     
     @property
+    def is_real(self) -> bool:
+        """ Checks if DDAE uses complex storage for any of defining arrays """
+        fields_to_check = ["_A", "_hA", "_B", "_hB",
+                           "_C", "_hC", "_D", "_hD"]
+        for field in fields_to_check:
+            val = getattr(self, field, None)
+            if val is not None:
+                if not np.isrealobj(val):
+                    return False
+        return True
+
+    @property
     def is_delay_difference_equation(self) -> bool:
         """ RDDE can NOT be delay difference equation """
         return False
     
-    def sort(self, inplace=False) -> 'RDDE':
-        """ Sorts delays (self.hA) into ascending order """
+    def to_asymptotic_transfer_function(self, **kwargs) -> 'RDDE':
         raise NotImplementedError("Not implemented yet")
+    
+    def sort(self, inplace=False) -> 'RDDE':
+        """ Sorts arrays containing matrices and delays (ascending order) """
+        
+        A, hA = sort_matrices_delays(self.A, self.hA)
+
+        if self.B is None or self.hB is None:
+            B, hB = self.B, self.hB
+        else:
+            B, hB = sort_matrices_delays(self.B, self.hB)
+        
+        if self.C is None or self.hC is None:
+            C, hC = self.C, self.hC
+        else:
+            C, hC = sort_matrices_delays(self.C, self.hC)
+        
+        if self.D is None or self.hD is None:
+            D, hD = self.D, self.hD
+        else:
+            D, hD = sort_matrices_delays(self.D, self.hD)
+
+        if inplace:
+            self._A = A
+            self._hA = hA
+            self._B = B
+            self._hB = hB
+            self._C = C
+            self._hC = hC
+            self._D = D
+            self._hD = hD
+        else:
+            return RDDE(A=A, hA=hA, B=B, hB=hB, C=C, hC=hC, D=D, hD=hD)
+    
     
     def compress(self, inplace=False, rtol=1e-5, atol=1e-8) -> 'RDDE':
         """ Removes delay duplicates, sorts delays into ascending order
@@ -118,5 +302,31 @@ class RDDE(TDSBase):
     def get_delay_difference_equation(self) -> None:
         """ RDDE has no associated delay difference equation """
         return None
+    
+    def eval_char_matrix(self, s: complex) -> npt.NDArray:
+        """ Evaluate characteristic matrix at `s`
 
+        M(s) = I*s - A[0]*exp(-s*hA[0]) - ... - A[mA]*exp(-s*hA[mA])
+        
+        Args:
+            s (complex, float, int): s from complex plane
+        
+        Returns:
+            M (array): characteristic matrix M evaluated at s
+        """
+        I = np.eye(self.n)
+        return I*s - np.sum(self.A * np.exp(-s*self.hA), axis=2)
+
+    def eval_char_matrix_derivative(self, s: complex) -> npt.NDArray:
+        """ Derivative of characteristic matrix with respect to s evaluated at s
+
+        dM(s) = I + hA[0]*A[0]*exp(-s*hA[0]) + ... + hA[mA]*A[mA]*exp(-s*hA[mA])
+        
+        Args:
+            s (complex, float, int): s from complex plane
+        
+        Returns:
+            dM (array): derivative of characteristic matrix M evaluated at s
+        """
+        return np.eye(self.n) + np.sum(self.A * self.hA * np.exp(-s*self.hA), axis=2)
 
