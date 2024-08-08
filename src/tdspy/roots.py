d9988e1718ea2ac906376080d414e09473e93705
Peichl Adam <adpeichl@gmail.com>
2024-08-08 11:15:50 +0200

    Fixed RMR, added docs to characteristic roots functionality

--- src/tdspy/roots.py
+++ src/tdspy/roots.py
@@ -31,17 +31,31 @@ def roots(tds: RDDE | NDDE | DDAE , r=0.0, **kwargs):
         r (int or list): region, specify r as number on real axis or rectangular
             region via 4 coordinates [Re_min, Re_max, Im_min, Im_max], default r=0.0
         kwargs:
-            max_size_evp (int): TODO, default 600
-            discretization (int): discretization, if None heuristic is envoked,
-                default None, keep default if you don't know, has to be > 1
-            basic_delay (float): define if delays are commensurate, default
-                None, used in discretization heuristic case `rhp`
+            discretization (int): discretization for discretizing DDAE into DAE,
+                optional, default None, if not specified, heuristic will be used
+                to obtain sufficient discretization
+            max_size_evp (int): maximum allowed size of eigenvalue problem (EVP)
+                optional, default 600
+            basic_delay (float): base delay in case delays are commensurate,
+                optional, default None, used in discretization heuristic
     
     Returns:
-            tuple containing
+        tuple containing
 
-                - roots (array): array of found roots
-                - metadata (RootsInfo): named tuple consisting of TODO
+            - cr (array): vector of obtained roots
+            - roots_info (RootsInfo): RMR metadata containing:
+                discretization (int): discretization used for obtaining EVP
+                gamma_r_exceeds_one (bool): flag indicating gamma(r) > 1
+                index_exceeds_one (bool): flag that index exceeds one
+                discretization_eigenvalues (array): eigenvalues of EVP
+                max_size_evp_enforced (bool): flag if maximum size of EVP was
+                    enforced
+                newton_inital_guesses (array): roots before newton corrections
+                newton_final_values (array): roots after newton corrections
+                newton_residuals (array): newton residuals
+                newton_unconverged_initial_guesses (array): mask of unconverged
+                    newton initial guesses
+                newton_large_corrections (array): mask of "large" corrections
     """
     # kwargs TODO
 
