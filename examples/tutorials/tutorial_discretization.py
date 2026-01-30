e0275d924479c348405c21dd48776cc90ab400ba
LockeErasmus <22911835+LockeErasmus@users.noreply.github.com>
2026-01-30 17:05:12 +0100

    Added PoC of discretization heuristic tutorial

--- examples/tutorials/tutorial_discretization.py
+++ examples/tutorials/tutorial_discretization.py
@@ -1,20 +1,23 @@
 """
-Discretization tutorial
-=======================
+Tutorial - Discretization Heuristic
+===================================
 
-In this tutorial, we will follow Example 5 from :cite:`wu2012reliably` to illustrate how
+In this tutorial, we will follow Example 1 from :cite:`wu2012reliably` to illustrate how
 to obtain number of discretization points such as to reliably compute all roots of LTI time-delay
-system in specifief right half-plane.
+system in specified right half-plane.
 
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.linalg as linalg
 import tdspy
 import tdspy.plot
 
-from tdspy.stability.discretization_heuristic import compute_n_rhp, incommensurate_gk
+from tdspy.stability.discretization_heuristic import compute_n_rhp, incommensurate_gk, cubic_spline_a, cubic_spline_b
 from tdspy.common.operations import shift_ddae, normalize_ddae
+
+from tdspy.common.discretization import discretize_ddae
 
 # %%
 # Note that as this is tutorial, we will go deeper than high level interface and
@@ -35,8 +38,12 @@ A1 = np.array([[3, 3, 3, 3],
 A = np.stack([A0, A1], axis=2) # This stacking along 3rd dimension is done automatically when using high level interface, it is for leveraging vectorization
 hA = np.array([0, 1.])
 
-n_grid = 20
-r = -1.5
+# Example 1
+E = np.eye(1)
+A0 = np.array([[3.2]])
+A1 = np.array([[-33.34]])
+A = np.stack([A0, A1], axis=2) # This stacking along 3rd dimension is done automatically when using high level interface, it is for leveraging vectorization
+hA = np.array([0, 1.])
 
 # Step 0: normalize by `hA[-1]` (maximal delay) and shift by `r` matrix representation, i.e.
 # perform :math:`s \leftarrow \frac{s}{\tau_{max}} + r` on the DDAE representation
@@ -47,24 +54,141 @@ r = -1.5
 # In this example, we have only one delay :math:`\tau_{1} = 1`, so normalization by maximal delay is already satisfied.
 # ------
 
-hmax = hA[-1]
-shift = -1.5
+shift = 0
 
+hmax = hA[-1]
+hK = hA / hmax
 K = A * np.exp(-shift * hmax * hA)
 K[:,:,0] -= E * shift * hmax
+
+print("Shifted and normalized matrices K_i:")
+
+for i in range(K.shape[2]):
+    print(f"K_{i} (hK_{i}={hK[i]}) = \n{K[:,:,i]}")
+
+# %%
+# Step 1: estimate region :math:`\Psi_{0}` and its boundary. We will use unnecesserily large
+# number of grid points for better visualization, however in practive much less points are needed.
+# ------
+
+n_grid = 1000
+
+gk = incommensurate_gk(E, K[:,:,0], K[:,:,1:], hK, grid_points=n_grid)
+gk = gk[np.isfinite(gk)] # get rid of inf and NaN
+points_Psi0 = gk[(np.real(gk) >= 0)]
+
+theta_points = np.abs(np.angle(points_Psi0))
+pp_a = cubic_spline_a(theta_points)
+pp_b = cubic_spline_b(theta_points)
+r_points = np.abs(points_Psi0)
+N_Psi0 = int( np.max( (r_points - pp_b) / pp_a ) + 1 ) # evalueate R(N, \theta) and get maxiumum
+
+# use this discretization to solve general eigenvalue problem
+E_evp, A_evp = discretize_ddae(E, K, hK, discretization=N_Psi0, s0=0)
+raw_roots_psi0 = linalg.eig(A_evp, E_evp, left=False, right=False)
+roots_psi0 = raw_roots_psi0[np.real(raw_roots_psi0) >= 0]
+
+Psi0 = np.r_[points_Psi0, np.conjugate(points_Psi0[::-1])]
+Psi0 = Psi0[np.real(Psi0) >= 0]
+
+plt.figure()
+plt.fill(np.real(Psi0), np.imag(Psi0), color="green", alpha=0.2,
+         label=r'$\Psi_0$')
+plt.plot(np.real(Psi0), np.imag(Psi0), color="green", linewidth=2,
+         label=r'$\Psi_0$ boundary')
+plt.scatter(np.real(roots_psi0), np.imag(roots_psi0), marker="+", color="red",
+            label=fr"roots with $\Re \geq 0$ for $N={N_Psi0}$")
+plt.legend()
+plt.show()
+
+# %%
+# As we are going to see, this discretization, while working fine, is very
+# conservative if the real part of roots is real. Continuing with the Example 1
+# we use :math:`\Psi_0` from above for the interval on real axis
+# :math:`[0, \kappa=1.93]` (Example 1 :cite:`wu2012reliably` - Equations (28)-
+# (30))
+# ------
+
+# %%
+kappa = 1.93
+
+# Equation (29)
+points_Psi0kappa = points_Psi0[np.real(points_Psi0) <= kappa]
+Psi0kappa = np.r_[points_Psi0kappa, np.conjugate(points_Psi0kappa[::-1])]
+
+# Equation (30) follows
+gk = incommensurate_gk(E, K[:,:,0], K[:,:,1:]*np.exp(-kappa * hK[1:]), hK, grid_points=n_grid)
+gk = gk[np.isfinite(gk)] # get rid of inf and NaN just in case
+
+points_Psikappa = gk[np.real(gk) >= kappa]
+Psikappa = np.r_[points_Psikappa, np.conjugate(points_Psikappa[::-1])]
+
+plt.figure()
+plt.fill(np.real(Psi0kappa), np.imag(Psi0kappa), color="green", alpha=0.2,
+         label=r'$\Psi_{[0,\kappa]}$')
+plt.fill(np.real(Psikappa), np.imag(Psikappa), color="blue", alpha=0.2,
+         label=r'$\Psi_{\kappa}$')
+# plt.plot(np.real(Psi0), np.imag(Psi0), color="green", linewidth=2,
+#          label=r'$\Psi_0$ boundary')
+plt.scatter(np.real(roots_psi0), np.imag(roots_psi0), marker="+", color="red",
+            label=fr"roots with $\Re \geq 0$ for $N={N_Psi0}$")
+plt.legend()
+plt.show()
+
+# %%
+# Let's see if we are able to decrease discretization without compromising
+# position of roots.
+# ------
+# %%
+
+# evalueate R(N, \theta)
+theta_points = np.abs(np.angle(points_Psi0kappa)) # |angle z|
+pp_a = cubic_spline_a(theta_points)
+pp_b = cubic_spline_b(theta_points)
+r_points = np.abs(points_Psi0kappa)
+N_Psi0kappa = int( np.max( (r_points - pp_b) / pp_a ) + 1 )
+
+theta_points = np.abs(np.angle(points_Psikappa)) # |angle z|
+pp_a = cubic_spline_a(theta_points)
+pp_b = cubic_spline_b(theta_points)
+r_points = np.abs(points_Psikappa)
+N_Psikappa = int( np.max( (r_points - pp_b) / pp_a ) + 1 )
+
+# as a result, take the the maximum with 8 as a floor value
+N = max(8, N_Psi0kappa, N_Psikappa)
+
+# use this discretization to solve general eigenvalue problem
+E_evp, A_evp = discretize_ddae(E, K, hK, discretization=N, s0=0)
+raw_roots = linalg.eig(A_evp, E_evp, left=False, right=False)
+roots = raw_roots[np.real(raw_roots) >= 0]
+
+plt.figure()
+plt.fill(np.real(Psi0kappa), np.imag(Psi0kappa), color="green", alpha=0.2,
+         label=r'$\Psi_{[0,\kappa]}$')
+plt.fill(np.real(Psikappa), np.imag(Psikappa), color="blue", alpha=0.2,
+         label=r'$\Psi_{\kappa}$')
+plt.plot(np.real(Psi0), np.imag(Psi0), color="green", linewidth=2,
+         label=r'$\Psi_0$ boundary')
+plt.scatter(np.real(roots_psi0), np.imag(roots_psi0), marker="+", color="red",
+            label=fr"roots with $\Re \geq 0$ for $N={N_Psi0}$ ")
+plt.scatter(np.real(roots), np.imag(roots), marker="o", color="black", 
+            facecolors="none", label=fr"roots with $\Re \geq 0$ for $N={N}$ ")
+plt.legend()
+plt.show()
+
+# %%
+plt.figure()
+plt.scatter(np.real(raw_roots_psi0), np.imag(raw_roots_psi0), marker="+", color="red",
+            label=fr"eigenvalues of EVP with $N={N_Psi0}$ ")
+plt.scatter(np.real(raw_roots), np.imag(raw_roots), marker="o", color="black", 
+            facecolors="none", label=fr"eigenvalues of EVP with $N={N}$ ")
+plt.axvline(0, color="black", label="boundary")
+plt.ylim(-100, 100)
+plt.xlim(-5, 5)
 
 
 # Note, in the case of neutral system, it is essential to check that half-plane
 # defined by `r` indeed contains finitely many roots, i.e. :math:`\gamma(r) < 1`.
-
-
-
-
-
-
-
-# %%
-
 
 
 
