aa35a2ef24ccba88b79c1921e9d65ba0cdf2c4cc
LockeErasmus <22911835+LockeErasmus@users.noreply.github.com>
2026-02-09 20:29:07 +0100

    Fixed char roots

--- src/tdspy/stability/characteristic_roots.py
+++ src/tdspy/stability/characteristic_roots.py
@@ -146,19 +146,7 @@ def roots_ddae(E: npt.NDArray, A: npt.NDArray, hA: npt.NDArray, r: float | list,
     # 2. real E, A, hA
     # 3. dimensions E.shape[:2] == A.shape[:2] and A.shape[2] == hA.shape[0]
 
-    # checks for region definition
-    if isinstance(r, (int, float)):
-        # all OK
-        case = "rhp"
-    elif isinstance (r, list):
-        assert len(r) == 4, "region has to be defined in form [a,b,c,d]"
-        assert r[0] < r[1] and r[2] < r[3], "region has to be defined as [a,b,c,d], a<b, c<d"
-        assert np.all(~np.isinf(r)), "region has to be finite rectangle"
-        case = "rect"
-    else:
-        raise ValueError(("Region (argument `r`) has to be defined as number, "
-                          "example `r=-5.1` or rectangular region, example "
-                          "`[-5, 10.5, 0, 100]`."))
+    # TODO checks for region definition or leave to HIGH level API???
 
     n = E.shape[0]
     if hA[0] > 0:
